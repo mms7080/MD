f1b0cd69cd9afe8daced2eb81dddc0400e89db58
Songyonggeun <yg38ant@gmail.com>
2025-09-06 00:52:18 +0900

    회원가입 entity /  회원가입 dto 생성 / db 연결 / 의존성 추가

---
+++ demo/src/main/java/com/example/demo/users/UsersDTO/UserDTO.java
@@ -0,0 +1,34 @@
+package com.example.demo.users.UsersDTO;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+
+    public interface Create {}
+    public interface Update {}
+
+    @NotBlank(message = "아이디는 필수 입력 항목입니다.", groups = Create.class)
+    @Pattern(regexp = "^[a-z0-9]{4,16}$", message = "아이디는 영문 소문자와 숫자로 4~16자이어야 합니다.", groups = Create.class)
+    private String username;
+
+    @NotBlank(message = "비밀번호는 필수 입력 항목입니다.", groups = Create.class)
+    @Size(min = 10, message = "비밀번호는 최소 10자 이상이어야 합니다.", groups = Create.class)
+    private String password; 
+
+    private String confirmPassword;
+
+    @NotBlank(message = "이름이 작성되지 않았습니다.", groups = Create.class)
+    private String name;
+
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
+    private String email; 
+}
