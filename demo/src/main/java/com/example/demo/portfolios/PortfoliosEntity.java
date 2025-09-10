2ebf20b1f20de70fba1d0058de6b31db52358a2d
Khh-25 <kimhoonhee25@naver.com>
2025-09-10 13:42:01 +0900

    khh 수정

--- demo/src/main/java/com/example/demo/portfolios/PortfoliosEntity.java
+++ demo/src/main/java/com/example/demo/portfolios/PortfoliosEntity.java
@@ -3,6 +3,10 @@ package com.example.demo.portfolios;
 
 import java.util.List;
 
+import lombok.Getter;
+
+@Getter
+// 임시 entity
 public class PortfoliosEntity {
     private String id;
     private String title;
@@ -14,8 +18,7 @@ public class PortfoliosEntity {
     private String desc;
 
     // 추가
-    private List<String> screenshots;
-    
+    private List<String> screenshots; 
     private List<TeamMemberEntity> team;
 
     private String icon; // 팀 아이콘
@@ -23,7 +26,7 @@ public class PortfoliosEntity {
     private String download; // 코드 다운로드 폴더 
     
 
-    // 생성자에 포함
+    // 생성자
     public PortfoliosEntity(String id, String title, String creator, List<String> tags, int likes,
                    String createdAt, String cover, String desc, List<String> screenshots,List<TeamMemberEntity> team
                    ,String icon, String link, String download) {
@@ -42,20 +45,7 @@ public class PortfoliosEntity {
         this.download = download;
     }
 
-    // Getter (Thymeleaf는 getter 메서드 기준으로 속성을 찾습니다)
-    public String getId() { return id; }
-    public String getTitle() { return title; }
-    public String getCreator() { return creator; }
-    public List<String> getTags() { return tags; }
-    public int getLikes() { return likes; }
-    public String getCreatedAt() { return createdAt; }
-    public String getCover() { return cover; }
-    public String getDesc() { return desc; }
-    public List<String> getScreenshots() { return screenshots; }
-    public List<TeamMemberEntity> getTeam() { return team; }
-    public String getIcon() { return icon; }
-    public String getLink() { return link; }
-    public String getDownload() { return download; }
+    
 
     public static void put(String string, PortfoliosEntity portfoliosEntity) {
         // TODO Auto-generated method stub
